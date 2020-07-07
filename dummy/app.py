bd54632f7485aa6069f8517b9a24ec1156a7be21
Syed Saif <sydsaifullah7@gmail.com>
2020-07-07 15:32:04 +0530

    Added router.py, which contains the class 'Router', to route URLs to view functions

--- dummy/app.py
+++ dummy/app.py
@@ -1,6 +1,6 @@
 #import werkzeug
 from werkzeug.wrappers import Request, Response
-from werkzeug.routing import Map, Rule
+from werkzeug.routing import Map
 from werkzeug.exceptions import HTTPException, NotFound
 from werkzeug.utils import redirect
 
---
+++ dummy/router.py
@@ -0,0 +1,39 @@
+from werkzeug.routing import Rule, Map
+
+
+class View_binder:
+	'''
+	The instance of this class is responsible to bind the users' view functions
+	to the Router object, which will in turn bind the functions to url_map of the app. 
+	''' 
+	def __init__(self, router):
+		self.router = router
+
+	def bind(self, view):
+		self.router.endpoint = view
+
+
+class Router:
+	'''
+	The class for creating the context manager to route URLs to user
+	defined functions
+	'''
+	
+
+	
+	def __init__(self, url_rule, methods = None):
+
+		self.rule = url_rule
+		self.methods = methods
+		self.endpoint = None
+
+	def __enter__(self):
+		
+		return View_binder(self)
+
+	def __exit__(self, *args):
+		if self.methods is None:
+			self.methods = ['GET']
+		rule_obj = Rule(self.rule, endpoint = self.endpoint.__name__, methods = self.methods)
+		Router.app.url_map.add(rule_obj)
+		Router.app.views[self.endpoint.__name__] = self.endpoint
